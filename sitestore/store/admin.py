8958b4432da1a74eb271ea4e56393266c2cbce1f
MaratGumerov <gumerovm74@gmail.com>
2023-05-12 22:35:36 +0300

    added admin tool fields

--- sitestore/store/admin.py
+++ sitestore/store/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import Tools
 
-admin.site.register(Tools)
-# Register your models here.
+class ToolsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'code', 'name','desc','created_at','updated_at',)
+    list_display_links = ('id', 'code', 'name')
+    search_fields = ('code', 'name')
+
+admin.site.register(Tools, ToolsAdmin)
+
