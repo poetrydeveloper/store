76aa0bb4c3637ef019cefaa9bab10562b520118c
MaratGumerov <gumerovm74@gmail.com>
2023-05-12 22:59:13 +0300

    added changed settings

--- sitestore/store/models.py
+++ sitestore/store/models.py
@@ -1,12 +1,16 @@
 from django.db import models
 
 class Tools(models.Model):
-    code = models.CharField(max_length=100)
-    name = models.TextField(max_length=250)
-    desc = models.TextField(max_length=250, blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    code = models.CharField(max_length=100, verbose_name='Артикул')
+    name = models.TextField(max_length=250, verbose_name='Название')
+    desc = models.TextField(max_length=250, blank=True, verbose_name='Описание')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Создан')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Изменен')
     photo = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
     
     def __str__(self):
-			  return self.title
+              return self.name
+    class Meta:
+        verbose_name = 'Инструмент'
+        verbose_name_plural = 'Инструменты'
+        
