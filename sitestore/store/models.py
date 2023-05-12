4a7e07cb4b0996e3a90dc0ade4cdb377d75e0eac
MaratGumerov <gumerovm74@gmail.com>
2023-05-12 14:05:47 +0300

    added str Tools

--- sitestore/store/models.py
+++ sitestore/store/models.py
@@ -7,3 +7,6 @@ class Tools(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     photo = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    
+    def __str__(self):
+			  return self.title
