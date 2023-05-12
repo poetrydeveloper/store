7f36534f3808de745481bc843cc8d284f0b19783
MaratGumerov <gumerovm74@gmail.com>
2023-05-12 23:47:26 +0300

    added Provider model

--- sitestore/store/admin.py
+++ sitestore/store/admin.py
@@ -1,10 +1,17 @@
 from django.contrib import admin
-from .models import Tools
+from .models import Tools, Provider
 
 class ToolsAdmin(admin.ModelAdmin):
-    list_display = ('id', 'code', 'name','desc','created_at','updated_at',)
+    list_display = ('id', 'code', 'provider', 'name','desc','created_at','updated_at',)
     list_display_links = ('id', 'code', 'name')
     search_fields = ('code', 'name')
+    list_filter = ('provider',)
+
+class ProviderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name',)
+    list_display_links = ('id', 'name')
+    search_fields = ('name',)
 
 admin.site.register(Tools, ToolsAdmin)
+admin.site.register(Provider, ProviderAdmin)
 
--- sitestore/store/models.py
+++ sitestore/store/models.py
@@ -7,10 +7,21 @@ class Tools(models.Model):
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Создан')
     updated_at = models.DateTimeField(auto_now=True, verbose_name='Изменен')
     photo = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    provider = models.ForeignKey('Provider', on_delete=models.PROTECT, null=True, verbose_name='Поставщик')
     
     def __str__(self):
               return self.name
     class Meta:
         verbose_name = 'Инструмент'
         verbose_name_plural = 'Инструменты'
-        
+
+class Provider(models.Model):
+    name = models.CharField(max_length=50, db_index=True, verbose_name='Поставщик')
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name = 'Поставщик'
+        verbose_name_plural = 'Поставщики'
+        ordering = ['name']
