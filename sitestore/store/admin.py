97ee15dff99f4fe64202566b9f5b00b7aa819a65
MaratGumerov <gumerovm74@gmail.com>
2023-05-14 22:22:51 +0300

    added Tags  added Category

--- sitestore/store/admin.py
+++ sitestore/store/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import Tools, Provider, Brand
+from .models import Tools, Provider, Brand, Category, Tag
 
 class ToolsAdmin(admin.ModelAdmin):
-    list_display = ('id', 'code', 'provider', 'brand', 'name','desc','created_at','updated_at',)
+    list_display = ('id', 'code', 'provider', 'brand', 'name','desc','created_at', 'photo','updated_at',)
     list_display_links = ('id', 'code', 'name')
     search_fields = ('code', 'name', 'brand')
     list_filter = ('provider', 'brand')
@@ -17,7 +17,21 @@ class BrandAdmin(admin.ModelAdmin):
     list_display_links = ('id', 'name')
     search_fields = ('name',)
 
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ("name",)}
+    list_display = ('id', 'name',)
+    list_display_links = ('id', 'name')
+    search_fields = ('name',)
+
+class TagAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ("name",)}
+    list_display = ('id', 'name',)
+    list_display_links = ('id', 'name')
+    search_fields = ('name',)
+
 admin.site.register(Tools, ToolsAdmin)
 admin.site.register(Provider, ProviderAdmin)
 admin.site.register(Brand, BrandAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Tag, TagAdmin)
 
