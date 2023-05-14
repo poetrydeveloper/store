b4661d3183c2ebc49eee78fac53373fb2c6b628c
MaratGumerov <gumerovm74@gmail.com>
2023-05-14 15:36:01 +0300

    added brand model

--- sitestore/store/admin.py
+++ sitestore/store/admin.py
@@ -1,17 +1,23 @@
 from django.contrib import admin
-from .models import Tools, Provider
+from .models import Tools, Provider, Brand
 
 class ToolsAdmin(admin.ModelAdmin):
-    list_display = ('id', 'code', 'provider', 'name','desc','created_at','updated_at',)
+    list_display = ('id', 'code', 'provider', 'brand', 'name','desc','created_at','updated_at',)
     list_display_links = ('id', 'code', 'name')
-    search_fields = ('code', 'name')
-    list_filter = ('provider',)
+    search_fields = ('code', 'name', 'brand')
+    list_filter = ('provider', 'brand')
 
 class ProviderAdmin(admin.ModelAdmin):
     list_display = ('id', 'name',)
     list_display_links = ('id', 'name')
     search_fields = ('name',)
 
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name',)
+    list_display_links = ('id', 'name')
+    search_fields = ('name',)
+
 admin.site.register(Tools, ToolsAdmin)
 admin.site.register(Provider, ProviderAdmin)
+admin.site.register(Brand, BrandAdmin)
 
--- sitestore/store/models.py
+++ sitestore/store/models.py
@@ -8,6 +8,7 @@ class Tools(models.Model):
     updated_at = models.DateTimeField(auto_now=True, verbose_name='Изменен')
     photo = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
     provider = models.ForeignKey('Provider', on_delete=models.PROTECT, null=True, verbose_name='Поставщик')
+    brand = models.ForeignKey('Brand', on_delete=models.PROTECT, null=True, verbose_name='Бренд')
     
     def __str__(self):
               return self.name
@@ -24,4 +25,15 @@ class Provider(models.Model):
     class Meta:
         verbose_name = 'Поставщик'
         verbose_name_plural = 'Поставщики'
+        ordering = ['name']
+
+class Brand(models.Model):
+    name = models.CharField(max_length=50, db_index=True, verbose_name='Бренд')
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name = 'Бренд'
+        verbose_name_plural = 'Бренды'
         ordering = ['name']
