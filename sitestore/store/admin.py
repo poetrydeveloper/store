f4370fd603a7dc2b770b82624ada5ed0400dddee
MaratGumerov <gumerovm74@gmail.com>
2023-05-14 22:35:45 +0300

    added get_photo to ToolsAdmin

--- sitestore/store/admin.py
+++ sitestore/store/admin.py
@@ -1,11 +1,19 @@
 from django.contrib import admin
 from .models import Tools, Provider, Brand, Category, Tag
+from django.utils.safestring import mark_safe
 
 class ToolsAdmin(admin.ModelAdmin):
-    list_display = ('id', 'code', 'provider', 'brand', 'name','desc','created_at', 'photo','updated_at',)
+    list_display = ('id', 'code', 'provider', 'brand', 'name','desc','created_at', 'get_photo','updated_at',)
     list_display_links = ('id', 'code', 'name')
     search_fields = ('code', 'name', 'brand')
-    list_filter = ('provider', 'brand')
+    list_filter = ('provider', 'brand', )
+
+    def get_photo(self, obj):
+        if obj.photo:
+            return mark_safe(f'<img src="{obj.photo.url}" wigth="50">')
+        return '-'
+    
+    get_photo.short_description = 'фото'
 
 class ProviderAdmin(admin.ModelAdmin):
     list_display = ('id', 'name',)
